3043363e91849e9a9ac8f0ccf171e81560ca3ba3
david <davidsk2004@gmail.com>
2026-01-12 22:55:30 +0100

    changed to original main

--- main.go
+++ main.go
@@ -1,31 +1,28 @@
 package main
 
 import (
-// "flag"
-// "fmt"
+	"flag"
+	"fmt"
 )
 
-//	func main() {
-//		idPtr := flag.Int64("id", 0, "id of the node")
-//		portPrt := flag.Int64("p", 9000, "port where the node will run")
-//		flag.Parse()
-//		id := *idPtr
-//		port := *portPrt
-//		fmt.Println("port", port)
-//		fmt.Println("id", id)
-//		url := fmt.Sprintf("localhost:%d", port)
-//		masterUrl := fmt.Sprintf("localhost:%d", 9000)
-//		if id == 0 {
-//			//add node to chain
-//			StartMasterServer(masterUrl, 0)
-//		} else if id < 5 {
-//			AddMsgBoardServer(url, masterUrl, id)
-//		} else {
-//			//start master node server
-//			Client(masterUrl)
-//
-//		}
-//	}
 func main() {
-	CraqRead()
+	idPtr := flag.Int64("id", 0, "id of the node")
+	portPrt := flag.Int64("p", 9000, "port where the node will run")
+	flag.Parse()
+	id := *idPtr
+	port := *portPrt
+	fmt.Println("port", port)
+	fmt.Println("id", id)
+	url := fmt.Sprintf("localhost:%d", port)
+	masterUrl := fmt.Sprintf("localhost:%d", 9000)
+	if id == 0 {
+		//add node to chain
+		StartMasterServer(masterUrl, 0)
+	} else if id < 5 {
+		AddMsgBoardServer(url, masterUrl, id)
+	} else {
+		//start master node server
+		Client(masterUrl)
+
+	}
 }
